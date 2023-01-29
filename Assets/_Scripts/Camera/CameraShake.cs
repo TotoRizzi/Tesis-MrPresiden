6b0e49dbea01132d6e64c6a6cb0800dbed0d3e62
TotoRizzi <83927597+TotoRizzi@users.noreply.github.com>
2023-01-29 12:45:21 -0300

    ad
    asd

--- Assets/_Scripts/Camera/CameraShake.cs
+++ Assets/_Scripts/Camera/CameraShake.cs
@@ -16,6 +16,11 @@ public class CameraShake : MonoBehaviour
         EnemyManager.instance.OnEnemyKilled += Shake;
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) Shake();
+    }
+
     public void Shake()
     {
         if (!_isInCoroutine) StartCoroutine(Shaking());
