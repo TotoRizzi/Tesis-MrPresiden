467581204c1785b00316c43e6856b9a871d5978a
santiago Koroch <90348800+BigDickPrograming@users.noreply.github.com>
2023-02-23 23:07:46 -0300

    luz Rota

--- Assets/Art/BrokenLight.cs
+++ Assets/Art/BrokenLight.cs
@@ -4,35 +4,33 @@ using UnityEngine;
 
 public class BrokenLight : MonoBehaviour
 {
-    public float minTime = 0.1f; // Minimum time interval for the object to turn on/off
-    public float maxTime = 0.5f; // Maximum time interval for the object to turn on/off
-    public GameObject objectToToggle; // Reference to the object to toggle
+    public float minTime = 0.07f;
+    public float maxTime = 0.3f;
 
-    private bool isOn = true; // Flag to keep track of the current state of the object
-    private float timer = 0f; // Timer to control the intervals between turning the object on/off
-    private float timeToToggle = 0f; // Time to toggle the object
+    public GameObject targetObject;
+
+    private bool isBlinking = false;
 
     void Start()
     {
-        // Calculate the first random interval to toggle the object
-        timeToToggle = Random.Range(minTime, maxTime);
+        StartCoroutine(Blink());
     }
 
-    void Update()
+    IEnumerator Blink()
     {
-        // Update the timer
-        timer += Time.deltaTime;
-
-        // Check if it's time to toggle the object
-        if (timer >= timeToToggle)
+        while (true)
         {
-            // Toggle the visibility of the object
-            isOn = !isOn;
-            objectToToggle.SetActive(isOn);
-
-            // Reset the timer and calculate a new random interval to toggle the object
-            timer = 0f;
-            timeToToggle = Random.Range(minTime, maxTime);
+            isBlinking = true;
+            targetObject.SetActive(!targetObject.activeSelf);
+            float blinkTime = Random.Range(minTime, maxTime);
+            yield return new WaitForSeconds(blinkTime);
+            isBlinking = false;
+            yield return null;
         }
+    }
+
+    public bool IsBlinking()
+    {
+        return isBlinking;
     }
 }
