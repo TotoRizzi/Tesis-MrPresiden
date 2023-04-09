eb5cb3cfe6325004ff8682bf5e1d9eec07e05ac4
Tatian05 <isp2014asje@gmail.com>
2023-04-09 18:00:23 -0300

    asd

--- Assets/_Scripts/LevelTimerManager.cs
+++ Assets/_Scripts/LevelTimerManager.cs
@@ -1,38 +1,40 @@
 using System.Collections;
 using UnityEngine;
+using System;
 public class LevelTimerManager : MonoBehaviour
 {
     [SerializeField] float _timer;
     [SerializeField] float _levelMaxTime;
     [SerializeField] float _timeToDiscount;
+    [SerializeField] bool _stopTrapMode;
     public float Timer { get { return _timer; } set { _timer = value; } }
     public float LevelMaxTime { get { return _levelMaxTime; } }
 
     bool _stopTimer;
+    bool _stopTrap;
 
-    public System.Action RedButton; //Lo llamo en el NextSceneOnTrigger
+    public Action RedButton;
     void Start()
     {
-        Helpers.GameManager.EnemyManager.OnEnemyKilled += () => EarnTime(_timeToDiscount);
-        RedButton += StopTimer;
+        Helpers.GameManager.EnemyManager.OnEnemyKilled += _stopTrapMode ? (Action)StopTrap : (Action)GoBackTrap;
+        RedButton += WinLevel;
         StartCoroutine(LevelTimer());
     }
     private void OnDisable()
     {
-        RedButton -= StopTimer;
-    }
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Alpha1)) RedButton();
+        RedButton -= WinLevel;
     }
     IEnumerator LevelTimer()
     {
+        WaitForSeconds wait = new WaitForSeconds(_timeToDiscount);
         while (_timer <= _levelMaxTime)
         {
             if (Helpers.GameManager.PauseManager.Paused) yield return null;
             else
             {
                 if (_stopTimer) yield break;
+                if (_stopTrap) yield return wait;
+                _stopTrap = false;
                 _timer += Time.deltaTime;
                 yield return null;
             }
@@ -40,13 +42,22 @@ public class LevelTimerManager : MonoBehaviour
         Helpers.GameManager.CinematicManager.PlayDefeatCinematic();
         Helpers.GameManager.PauseManager.PauseObjectsInCinematic();
     }
-    public void StopTimer()
+    public void WinLevel()
     {
         _stopTimer = true;
-        Helpers.GameManager.EnemyManager.OnEnemyKilled -= () => EarnTime(_timeToDiscount);
+        Helpers.GameManager.EnemyManager.OnEnemyKilled -= GoBackTrap;
     }
-    public void EarnTime(float time)
+
+    void GoBackTrap()
+    {
+        EarnTime(_timeToDiscount);
+    }
+    void EarnTime(float time)
     {
         _timer -= time;
+    }
+    void StopTrap()
+    {
+        _stopTrap = true;
     }
 }
