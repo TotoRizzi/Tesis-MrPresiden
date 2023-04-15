98902a6434cbf610ca8c6a2ab4a5840cad42f5a4
Tatian05 <isp2014asje@gmail.com>
2023-04-15 13:41:26 -0300

    asd

--- Assets/_Scripts/LevelTimerManager.cs
+++ Assets/_Scripts/LevelTimerManager.cs
@@ -1,15 +1,11 @@
 using System.Collections;
 using UnityEngine;
-using UnityEngine.Experimental.Rendering.Universal;
 using System;
 public class LevelTimerManager : MonoBehaviour
 {
     [SerializeField] float _timer;
     [SerializeField] float _levelMaxTime;
     [SerializeField] float _timeToDiscount;
-    [SerializeField] Light2D[] _levelLights;
-    [SerializeField] Light2D _globalLight;
-    [SerializeField] Animator _levelLightsAnimator;
     public float Timer { get { return _timer; } set { _timer = value; } }
     public float LevelMaxTime { get { return _levelMaxTime; } }
 
@@ -31,8 +27,6 @@ public class LevelTimerManager : MonoBehaviour
     IEnumerator LevelTimer()
     {
         if (_firstTime) yield break;
-        //_levelLightsAnimator.Play("LevelLight_Open");
-        //_globalLight.color = Color.red;
         _firstTime = true;
         WaitForSeconds wait = new WaitForSeconds(_timeToDiscount);
         while (_timer <= _levelMaxTime)
@@ -41,18 +35,12 @@ public class LevelTimerManager : MonoBehaviour
             else
             {
                 if (_stopTimer) yield break;                            //Cuando pongo pausa
-                if (_stopTrap)
-                {
-                   // _globalLight.color = Color.white;
-                    yield return wait;                                  //Cuando muere un enemigo
-                    //_levelLightsAnimator.SetBool("_stopLights", false);
-                }
+                if (_stopTrap) yield return wait;                                  //Cuando muere un enemigo
                 _stopTrap = false;
                 _timer += Time.deltaTime;
                 yield return null;
             }
         }
-        //_levelLightsAnimator.SetBool("_stopLights", true);
         Helpers.GameManager.CinematicManager.PlayDefeatCinematic();
         Helpers.GameManager.PauseManager.PauseObjectsInCinematic();
     }
@@ -63,7 +51,6 @@ public class LevelTimerManager : MonoBehaviour
     }
     void StopTrap()
     {
-        _levelLightsAnimator.SetBool("_stopLights", true);
         _stopTrap = true;
     }
 }
