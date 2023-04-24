600b4c029bd324f8614a341e395325793ea44707
SlendyBones <62655868+SlendyBones@users.noreply.github.com>
2023-04-24 14:25:14 -0300

    Parallax Scrip

---
+++ Assets/_Scripts/Parallax.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    public Camera cam;
+
+    public Transform subject;
+
+    Vector2 startPos;
+
+    float startZ;
+
+    Vector2 travel => (Vector2)cam.transform.position - startPos;
+
+    
+
+    float distanceFromSubject => transform.position.z - subject.position.z;
+
+    float clippingPlane => (cam.transform.position.z + (distanceFromSubject > 0 ? cam.farClipPlane : cam.nearClipPlane));
+
+    float parallaxFactor => Mathf.Abs(distanceFromSubject) / clippingPlane;
+
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        Vector2 newPos = startPos + travel * parallaxFactor;
+        transform.position = new Vector3(newPos.x, newPos.y, startZ);
+    }
+}
