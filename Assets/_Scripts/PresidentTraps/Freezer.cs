610808c4f82c1c88bd9693b1c915438f917cc0e7
Tatian05 <isp2014asje@gmail.com>
2023-05-02 13:14:19 -0300

    asd

---
+++ Assets/_Scripts/PresidentTraps/Freezer.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using UnityEngine;
+
+public class Freezer : MonoBehaviour
+{
+    [SerializeField] ParticleSystem _freezerPS;
+    [SerializeField] GameObject _president;
+    [SerializeField] SpriteRenderer[] _presidentSprites;
+    [SerializeField] SpriteRenderer _ice;
+    [SerializeField] Color _targetColor;
+    Color _iceStartColor, _onColorChange, _onIceColorChange;
+
+    Animator _presidentAnimator;
+    float _animSpeed, _levelMaxTime, _wonTimer;
+    System.Action _freezerAction;
+    private void Start()
+    {
+        Helpers.GameManager.EnemyManager.OnEnemyKilled += () => StartCoroutine(StopFreezer());
+        Helpers.LevelTimerManager.OnLevelStart += () => _freezerPS.Play();
+
+        _presidentAnimator = _president.GetComponent<Animator>();
+        _levelMaxTime = Helpers.LevelTimerManager.LevelMaxTime;
+
+        RuntimeAnimatorController ac = _presidentAnimator.runtimeAnimatorController;
+        foreach (var anim in ac.animationClips)
+            if (anim.name == "Level 10") _animSpeed = anim.length;
+
+        _iceStartColor = _ice.color;
+
+        _freezerAction = FreezerOn;
+    }
+    void Update()
+    {
+        _freezerAction();
+    }
+    IEnumerator StopFreezer()
+    {
+        WaitForSeconds wait = new WaitForSeconds(1);
+        _freezerPS.Stop();
+        yield return wait;
+        _freezerPS.Play();
+    }
+
+    void FreezerOn()
+    {
+        _presidentAnimator.SetFloat("Speed", Mathf.Lerp(_animSpeed, 0, Helpers.LevelTimerManager.Timer / _levelMaxTime));
+
+        for (int i = 0; i < _presidentSprites.Length; i++)
+            _presidentSprites[i].color = Color.Lerp(Color.white, _targetColor, Helpers.LevelTimerManager.Timer / _levelMaxTime);
+
+
+        _ice.color = new Color(_iceStartColor.r, _iceStartColor.g, _iceStartColor.b, Mathf.Lerp(0, 1, Helpers.LevelTimerManager.Timer / _levelMaxTime));
+    }
+
+    void Won()
+    {
+        _wonTimer += Time.deltaTime;
+        for (int i = 0; i < _presidentSprites.Length; i++)
+            _presidentSprites[i].color = Color.Lerp(_onColorChange, Color.white, _wonTimer / 1);
+
+        _ice.color = new Color(_onIceColorChange.r, _onIceColorChange.g, _onIceColorChange.b, Mathf.Lerp(_onIceColorChange.a, 0, _wonTimer / 1));
+    }
+
+    public void PlayerWon()
+    {
+        _onColorChange = _presidentSprites[0].color;
+        _onIceColorChange = _ice.color;
+        _freezerAction = Won;
+    }
+}
