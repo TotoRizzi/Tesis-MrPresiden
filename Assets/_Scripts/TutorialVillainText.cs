4cb107289eb6602dd0ec1276736fc9de69daa734
Tatian05 <isp2014asje@gmail.com>
2023-06-02 15:18:01 -0300

    asdasd

--- Assets/_Scripts/TutorialVillainText.cs
+++ Assets/_Scripts/TutorialVillainText.cs
@@ -6,14 +6,26 @@ using TMPro;
 public class TutorialVillainText : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI _textDispay;
+    [SerializeField] Transform[] _eyes;
     [SerializeField] float _typingSpeed;
+    [SerializeField] float _smooth;
     string _sentence;
 
+    Transform _player;
+    Vector3[] _initialPos;
     private void Start()
     {
         StartCoroutine(Type());
+        _player = Helpers.GameManager.Player.transform;
+        _initialPos = new Vector3[2] { _eyes[0].position, _eyes[1].position };
     }
+    private void LateUpdate()
+    {
+        Vector3 distance = _player.position - transform.position;
 
+        for (int i = 0; i < _eyes.Length; i++)
+            _eyes[i].position = Vector3.Lerp(_eyes[i].position, new Vector3(Mathf.Clamp(distance.x, _initialPos[i].x  + -.25f, _initialPos[i].x + .25f), Mathf.Clamp(distance.y, _initialPos[i].y + - .35f, _initialPos[i].x + .35f), _eyes[i].position.z), _smooth * Time.deltaTime);
+    }
     IEnumerator Type()
     {
         var wait = new WaitForSeconds(_typingSpeed);
