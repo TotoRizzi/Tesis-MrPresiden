6bcdba55809f2f8a91f8b75e9bf45394cceed860
TotoRizzi <83927597+TotoRizzi@users.noreply.github.com>
2023-06-07 15:16:03 -0300

    s
    s

--- Assets/_Scripts/_Player/GroundCheck.cs
+++ Assets/_Scripts/_Player/GroundCheck.cs
@@ -7,6 +7,11 @@ public class GroundCheck : MonoBehaviour
     private bool _isGrounded;
     public bool IsGrounded { get { return _isGrounded; } private set { } }
 
+    private void Start()
+    {
+        Helpers.GameManager.OnPlayerDead += () => _isGrounded = true;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         _isGrounded = true;
