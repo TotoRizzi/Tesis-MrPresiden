64b523de3e1c873ae55f04af6d16154b3533d0cd
Tatian05 <isp2014asje@gmail.com>
2023-06-08 16:50:09 -0300

    BotonPlay

--- Assets/_Scripts/Managers/MenuManager.cs
+++ Assets/_Scripts/Managers/MenuManager.cs
@@ -1,11 +1,23 @@
 using UnityEngine;
+using UnityEngine.Playables;
+
 public class MenuManager : MonoBehaviour
 {
+    [SerializeField] PlayableDirector _trainCinematic;
     public void BTN_Play()
     {
         var gameMode = Helpers.GameManager.SaveDataManager.GetInt(Helpers.GameManager.gameModeSaveName, (int)GameMode.EasyGameMode);
+        bool trainCinematic = PlayerPrefs.HasKey("TrainCinematic");
+
         PlayerPrefs.DeleteAll();
         Helpers.GameManager.SaveDataManager.SaveInt(Helpers.GameManager.gameModeSaveName, gameMode);
+
+        if (trainCinematic) Helpers.GameManager.LoadSceneManager.LoadLevel("Level 0.1");
+        else
+        {
+            _trainCinematic.Play();
+            PlayerPrefs.SetString("TrainCinematic", "TrainCinematic");
+        }
     }
     public void BTN_Credits()
     {
