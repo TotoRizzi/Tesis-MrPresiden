8fe848cae3a458303c30c064cb06e6156a3e0871
Tatian05 <isp2014asje@gmail.com>
2023-06-17 18:12:22 -0300

    Skip principal cinematica y arreglo botones

--- Assets/_Scripts/SkipCinematic.cs
+++ Assets/_Scripts/SkipCinematic.cs
@@ -1,14 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
 public class SkipCinematic : MonoBehaviour
 {
+    [SerializeField] bool _principalCinematic;
     void Update()
     {
         if (Input.anyKey)
         {
-            Helpers.GameManager.CinematicManager.SkipDefeatCinematic();
+            if (_principalCinematic) Helpers.GameManager.LoadSceneManager.LoadLevel("Level 0 Tutorial");
+            else Helpers.GameManager.CinematicManager.SkipDefeatCinematic();
             gameObject.SetActive(false);
         }
     }
