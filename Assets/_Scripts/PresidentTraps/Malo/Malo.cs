f18df5b79c06e3afb2a28636357b5988c639a2a8
Tatian05 <isp2014asje@gmail.com>
2023-08-02 17:15:22 -0300

    asd

--- Assets/_Scripts/PresidentTraps/Malo/Malo.cs
+++ Assets/_Scripts/PresidentTraps/Malo/Malo.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 public class Malo : MonoBehaviour
 {
     Animator _animator;
-    [SerializeField] string _winAnimationName, _onEnemyKilledAnimation;
+    [SerializeField] string _winAnimationName, _onEnemyKilledAnimation, _levelStartAnimName;
+    [SerializeField] bool _setAnimOnLevelStart = false;
     void Start()
     {
         _animator = GetComponent<Animator>();
+        if (_setAnimOnLevelStart) Helpers.LevelTimerManager.OnLevelStart += delegate { _animator.Play(_levelStartAnimName); };
         Helpers.GameManager.EnemyManager.OnEnemyKilled += () => _animator.Play(_onEnemyKilledAnimation);
         Helpers.LevelTimerManager.OnLevelDefeat += () => _animator.Play(_winAnimationName);
     }
