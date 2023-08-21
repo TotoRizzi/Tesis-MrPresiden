f60a2bf19ea45ea9745b3c80eb5778042dcfe960
Tatian05 <isp2014asje@gmail.com>
2023-08-21 10:32:38 -0300

    Pathfinding arreglado

--- Assets/_Scripts/Enemies & Traps/Drones/Enemy_FollowDrone.cs
+++ Assets/_Scripts/Enemies & Traps/Drones/Enemy_FollowDrone.cs
@@ -1,47 +1,30 @@
 using UnityEngine;
+using UnityEngine.AI;
 public class Enemy_FollowDrone : Enemy
 {
     enum DroneStates { Idle, Follow, Pathfinding };
     [SerializeField] protected float _speed = 1f;
 
     EventFSM<DroneStates> _myFsm;
+    NavMeshAgent _navMeshAgent;
     public override void Start()
     {
         base.Start();
-        Movement_BasicFollowTarget followTarget = new Movement_BasicFollowTarget(transform, Helpers.GameManager.Player.transform, _speed);
-        PathfindingMovement pathfindingMovement = new PathfindingMovement(transform, Helpers.GameManager.Player.transform, _speed);
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        _navMeshAgent.enabled = true;
+        _navMeshAgent.updateRotation = false;
+        _navMeshAgent.updateUpAxis = false;
+        _navMeshAgent.speed = _speed;
+        var playerTransform = Helpers.GameManager.Player.transform;
 
         var idle = new State<DroneStates>("Idle");
         var follow = new State<DroneStates>("Follow");
-        var pathfinding = new State<DroneStates>("Pathfinding");
 
         StateConfigurer.Create(idle).SetTransition(DroneStates.Follow, follow).Done();
-        StateConfigurer.Create(follow).SetTransition(DroneStates.Pathfinding, pathfinding).Done();
-        StateConfigurer.Create(pathfinding).SetTransition(DroneStates.Idle, idle).SetTransition(DroneStates.Follow, follow).Done();
+        StateConfigurer.Create(follow).Done();
 
         idle.OnUpdate += delegate { if (CanSeePlayer()) _myFsm.SendInput(DroneStates.Follow); };
-        follow.OnUpdate += delegate
-        {
-            if (!CanSeePlayer()) _myFsm.SendInput(DroneStates.Pathfinding);
-            followTarget.Move();
-        };
-
-        float counter = 0;
-        pathfinding.OnEnter += x =>
-        {
-            pathfindingMovement.SetAllNodes(FindObjectsOfType<Node>());
-            pathfindingMovement.SetPath();
-        };
-
-        pathfinding.OnUpdate += delegate
-        {
-            pathfindingMovement.Move();
-            counter += Time.deltaTime;
-            if (counter >= 3f) _myFsm.SendInput(DroneStates.Idle);
-
-            if (CanSeePlayer()) _myFsm.SendInput(DroneStates.Follow);
-        };
-        pathfinding.OnExit += x => counter = 0;
+        follow.OnUpdate += delegate { _navMeshAgent.SetDestination(playerTransform.position); };
 
         _myFsm = new EventFSM<DroneStates>(idle);
     }
--- Assets/_Scripts/Movement/Movement_BasicFollowTarget.cs
+++ Assets/_Scripts/Movement/Movement_BasicFollowTarget.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using UnityEngine;
 public class Movement_BasicFollowTarget : IMovement
 {
@@ -19,86 +18,5 @@ public class Movement_BasicFollowTarget : IMovement
         Vector3 dir = ((_target.position + _offSet) - _transform.position).normalized;
 
         _transform.position += dir * _speed * Time.deltaTime;
-    }
-}
-
-public class PathfindingMovement : IMovement
-{
-    Transform _transform, _target;
-    float _speed;
-
-    AStar _aStar;
-    List<Node> _path = new List<Node>();
-    Node[] _allNodes;
-    Node _startingNode, _goalNode;
-    LayerMask _wallLayer;
-    public PathfindingMovement(Transform transform, Transform target, float speed)
-    {
-        _transform = transform;
-        _target = target;
-        _speed = speed;
-
-        _wallLayer = LayerMask.GetMask("Border");
-    }
-    public void Move()
-    {
-        Vector3 dir = _path[0].transform.position - _transform.position;
-        _transform.position += dir.normalized * _speed * Time.deltaTime;
-
-        if (dir.magnitude < 0.1f)
-            _path.RemoveAt(0);
-    }
-    public void SetPath()
-    {
-        _aStar = new AStar();
-        if (_path == null) return;
-        _path = _aStar.ConstructPathAStar(GetStartingNode(), GetGoalNode(_target.position));
-        if (InSight(GetStartingNode().transform.position, _transform.position, _wallLayer)) _path.Remove(GetStartingNode());
-    }
-    public void SetAllNodes(Node[] allNodes)
-    {
-        _allNodes = allNodes;
-    }
-    bool InSight(Vector3 start, Vector3 end, LayerMask layer)
-    {
-        return Physics2D.Raycast(start, end, layer);
-    }
-    Node GetStartingNode()
-    {
-        float minValue = 0;
-        for (int i = 0; i < _allNodes.Length; i++)
-        {
-            float distance = Vector3.Distance(_allNodes[i].transform.position, _transform.position);
-            if (minValue == 0)
-            {
-                minValue = distance;
-                _startingNode = _allNodes[i];
-            }
-            else if (distance < minValue)
-            {
-                minValue = distance;
-                _startingNode = _allNodes[i];
-            }
-        }
-        return _startingNode;
-    }
-    Node GetGoalNode(Vector3 goalPos)
-    {
-        float minValue = 0;
-        for (int i = 0; i < _allNodes.Length; i++)
-        {
-            float distance = Vector3.Distance(goalPos, _allNodes[i].transform.position);
-            if (minValue == 0)
-            {
-                minValue = distance;
-                _goalNode = _allNodes[i];
-            }
-            else if (distance < minValue)
-            {
-                minValue = distance;
-                _goalNode = _allNodes[i];
-            }
-        }
-        return _goalNode;
     }
 }
